08107c81ef8d8948386ae57737eb75350f1e3a43
shilili <18262272638@163.com>
2025-12-08 14:53:34 +0800

    feat: 引入基础例子

--- apps/slate/src/components/SlateEditorDemo/index.vue
+++ apps/slate/src/components/SlateEditorDemo/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    demo
-  </div>
+  <Slate :editor="editor">
+    <Editable :placeholder="'输入一些文本...'" />
+  </Slate>
 </template>
 
-<script setup lang="ts">
-  
+<script setup>
+import { Slate, Editable } from 'slate-vue3'
+import { createEditor } from 'slate-vue3/core'
+import { withHistory } from 'slate-vue3/history'
+import { withDOM } from 'slate-vue3/dom'
+
+const editor = withHistory(withDOM(createEditor()))
+editor.children = [
+  { type: 'paragraph', children: [{ text: 'Hello Slate' }] },
+]
 </script>
