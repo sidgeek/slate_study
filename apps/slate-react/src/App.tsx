6d07d5a0abfaab5b932447f8192b206ec8032681
shilili <18262272638@163.com>
2025-12-08 17:35:33 +0800

    feat: 移除干扰

--- apps/slate-react/src/App.tsx
+++ apps/slate-react/src/App.tsx
@@ -4,7 +4,6 @@ import { BasicEditor } from './BasicEditor'
 export default function App() {
   return (
     <div style={{ padding: 16 }}>
-      <h3>Slate React 最简 Demo</h3>
       <BasicEditor />
     </div>
   )
